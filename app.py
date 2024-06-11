7ad003a9c2a95e94b33effa378ed3746d7f68fc7
Atharvapuranik12 <83014186+Atharvapuranik12@users.noreply.github.com>
2024-06-11 16:14:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, send_file
 import cv2
 import numpy as np
-from tensorflow.keras.models import model_from_json
+from keras.models import model_from_json
 import os
+from io import BytesIO
+from PIL import Image
 
 app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000))
 
 # Load Face Detection Model
-face_cascade_path = os.path.join('models', 'haarcascade_frontalface_default.xml')
+face_cascade_path = r"models/haarcascade_frontalface_default.xml"
 face_cascade = cv2.CascadeClassifier(face_cascade_path)
 if face_cascade.empty():
     print(f"Error loading face cascade from {face_cascade_path}")
 
 # Load Anti-Spoofing Model
-model_json_path = os.path.join('models', 'pro_antispoofing_model_mobilenet.json')
-model_weights_path = os.path.join('models', 'antispoofing_model.h5')
+model_json_path = r'models/antispoofing_model.json'
+model_weights_path = r'models/antispoofing_model.h5'
 
 with open(model_json_path, 'r') as json_file:
     loaded_model_json = json_file.read()
@@ -74,9 +77,20 @@ def generate_frames():
 def index():
     return render_template('index.html')
 
-@app.route('/video_feed')
+@app.route('/video_feed', methods=['POST'])
 def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    if 'frame' not in request.files:
+        return "No frame found", 400
+
+    frame = request.files['frame'].read()
+    npimg = np.frombuffer(frame, np.uint8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+    processed_frame = detect_and_predict(img)
+    _, buffer = cv2.imencode('.jpg', processed_frame)
+    io_buf = BytesIO(buffer)
+    return send_file(io_buf, mimetype='image/jpeg')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
